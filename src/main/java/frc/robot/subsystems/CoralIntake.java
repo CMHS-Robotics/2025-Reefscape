30f69a857b1d1120d61764846b3faaf4fb33b068
TheDarkWarrior115 <115947724+TheDarkWarrior115@users.noreply.github.com>
2025-02-15 11:04:07 -0700

    sparks

--- src/main/java/frc/robot/subsystems/CoralIntake.java
+++ src/main/java/frc/robot/subsystems/CoralIntake.java
@@ -6,7 +6,9 @@ import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.revrobotics.jni.CANSparkJNI;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -14,34 +16,27 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class CoralIntake implements Subsystem {
     CommandXboxController Manipulator;
-    int CoralContinuousMotorId = 15;
-    TalonFX CoralContinuousMotor = new TalonFX(CoralContinuousMotorId);
-    CANSparkJNI NeoWristMotor = new CANSparkJNI();
+    int CoralSpinMotorId = 15;
+    int CoralWristMotorId = 16;
+    Spark CoralSpin = new Spark(CoralSpinMotorId);
+    Spark CoralWrist = new Spark(CoralWristMotorId);
     
 
     public CoralIntake (CommandXboxController x){
         Manipulator = x;
 
-        var config = new TalonFXConfiguration();
-        config.MotorOutput.NeutralMode = NeutralModeValue.Brake;
-        config.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
-        config.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RotorSensor;
-        config.Feedback.SensorToMechanismRatio = 1;
-        config.Voltage.PeakForwardVoltage = 3;
-        config.Voltage.PeakReverseVoltage = 3;
-
-        CoralContinuousMotor.getConfigurator().apply(config);
-
+        var config = new SparkMaxConfig();
+        
         Trigger leftBumper = Manipulator.leftBumper();
         Trigger rightBumper = Manipulator.rightBumper();
         this.setDefaultCommand(Commands.run(()->{
 
             if(leftBumper.getAsBoolean()){
-                CoralContinuousMotor.set(0.5);
+                CoralSpin.set(0.5);
             }else if(rightBumper.getAsBoolean()){
-                CoralContinuousMotor.set(-0.5);
+                CoralSpin.set(-0.5);
             }else{
-                CoralContinuousMotor.set(0);
+                CoralSpin.set(0);
             }
 
         },this));   

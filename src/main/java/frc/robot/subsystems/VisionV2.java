81a9766468864422f1afe7d5960f83e7fa090b59
WinslowF <winslowfegley@gmail.com>
2025-11-11 17:10:36 -0700

    chatGPT slop code

--- src/main/java/frc/robot/subsystems/VisionV2.java
+++ src/main/java/frc/robot/subsystems/VisionV2.java
@@ -1,63 +1,69 @@
 package frc.robot.subsystems;
-//20 deg angle 
-
-import java.io.Console;
-import java.lang.reflect.Array;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
-import edu.wpi.first.networktables.NetworkTableEntry;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.tools.DashboardSuite;
-import frc.robot.subsystems.Elevator;
-
-
-
 
 public class VisionV2 extends SubsystemBase {
-    PhotonCamera FrontLeftCamera = new PhotonCamera("FrontLeftCamera");
-    PhotonCamera FrontRightCamera = new PhotonCamera("FrontRightCamera");
-    PhotonTrackedTarget targetL;
-    PhotonTrackedTarget targetR;
-    int targetIDR;
-    int targetIDL;
-    boolean targetsMatch;
-    public VisionV2(CommandSwerveDrivetrain s, CommandXboxController d){
-        CommandSwerveDrivetrain swerve = s;
-        CommandXboxController driver = d;
+
+    // Cameras
+    private PhotonCamera FrontLeftCamera = new PhotonCamera("FrontLeftCamera");
+    private PhotonCamera FrontRightCamera = new PhotonCamera("FrontRightCamera");
+
+    // Stored results
+    private PhotonTrackedTarget targetL;
+    private PhotonTrackedTarget targetR;
+    private int targetIDL = -1;
+    private int targetIDR = -1;
+    private boolean targetsMatch = false;
+
+    // Swerve + Driver (kept in case you need later)
+    private final CommandSwerveDrivetrain swerve;
+    private final CommandXboxController driver;
+
+    public VisionV2(CommandSwerveDrivetrain s, CommandXboxController d) {
+        this.swerve = s;
+        this.driver = d;
     }
-    public int GetCameraTarget(){
+
+    public int GetCameraTarget() {
         var resultL = FrontLeftCamera.getLatestResult();
         var resultR = FrontRightCamera.getLatestResult();
 
-        if(resultL.hasTargets() || resultR.hasTargets()){
-            PhotonTrackedTarget targetL = resultL.getBestTarget();
-            PhotonTrackedTarget targetR = resultR.getBestTarget();
-            int targetIDR = targetR.getFiducialId();
-            int targetIDL = targetL.getFiducialId();
-            
-        }
-        if(targetIDL == targetIDR){
-            targetsMatch=true;
-        }else{
+        if (resultL.hasTargets()) {
+            targetL = resultL.getBestTarget();
+            targetIDL = targetL.getFiducialId();
+        } else {
             targetIDL = -1;
-            targetsMatch=false;
         }
-        
+
+        if (resultR.hasTargets()) {
+            targetR = resultR.getBestTarget();
+            targetIDR = targetR.getFiducialId();
+        } else {
+            targetIDR = -1;
+        }
+
+        // Match check
+        if (targetIDL != -1 && targetIDL == targetIDR) {
+            targetsMatch = true;
+        } else {
+            targetsMatch = false;
+        }
+
         return targetIDL;
-        
     }
 
     @Override
-    public void periodic(){
+    public void periodic() {
         GetCameraTarget();
+
         SmartDashboard.putBoolean("HasSameTargetId", targetsMatch);
-        SmartDashboard.putNumber("SameTargetId", targetIDR);
+        SmartDashboard.putNumber("LeftTargetID", targetIDL);
+        SmartDashboard.putNumber("RightTargetID", targetIDR);
+        SmartDashboard.putNumber("VerifiedTargetID", targetsMatch ? targetIDL : -1);
     }
 }

197f842be63fe3e924eede2369d1f453fb09f63f
WinslowF <winslowfegley@gmail.com>
2025-11-13 16:23:40 -0700

    Vision V2 calcRotation fix and a robotMoveToTag V2 using the transform 3D class

--- src/main/java/frc/robot/subsystems/VisionV2.java
+++ src/main/java/frc/robot/subsystems/VisionV2.java
@@ -3,6 +3,10 @@ package frc.robot.subsystems;
 import org.photonvision.PhotonCamera;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+
+// added this??
+import edu.wpi.first.math.geometry.Transform3d;
+
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -26,6 +30,8 @@ public class VisionV2 extends SubsystemBase {
     private int targetIDR = -1;
     private boolean targetsMatch = false;
 
+    private final AprilTagFieldLayout fieldLayout;
+
     // Swerve + Driver (kept in case you need later)
     private final CommandSwerveDrivetrain swerve;
     private final CommandXboxController driver;
@@ -33,6 +39,8 @@ public class VisionV2 extends SubsystemBase {
     public VisionV2(CommandSwerveDrivetrain s, CommandXboxController d) {
         this.swerve = s;
         this.driver = d;
+
+        fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
     }
 
     public int GetCameraTarget() {
@@ -62,13 +70,44 @@ public class VisionV2 extends SubsystemBase {
 
         return targetIDL;
     }
-    public Rotation2d CalcRotation(PhotonTrackedTarget target){
-        Rotation2d robotHeading = swerve.getState().Pose.getRotation();
-        Rotation2d targHeading = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded).getTags().get(target.getFiducialId()).pose.getRotation().toRotation2d();
 
-        Rotation2d RotError = targHeading.minus(robotHeading);
-        return RotError;
+    // this causes code to have a fatal error :(
+
+    public Rotation2d CalcRotation(PhotonTrackedTarget target) {
+        // If there’s no target passed in, just return 0° rotation error
+        if (target == null) {
+            return Rotation2d.fromDegrees(0);
+        }
+
+        AprilTagFieldLayout fieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
+    
+        int seenTagId = target.getFiducialId();
+    
+        var tagMap = fieldLayout.getTags();
+    
+        var tagDefinition = tagMap.get(seenTagId);
+
+        if (tagDefinition == null) {
+            return Rotation2d.fromDegrees(0);
+        }
+        Rotation2d robotHeading = swerve.getState().Pose.getRotation();
+
+        Rotation2d tagHeading = tagDefinition.pose.getRotation().toRotation2d();
+
+        Rotation2d rotationError = tagHeading.minus(robotHeading);
+    
+        return rotationError;
     }
+
+    public Transform3d getTagTransform() {
+        var result = FrontLeftCamera.getLatestResult();
+        if (!result.hasTargets()) return null;
+    
+        PhotonTrackedTarget target = result.getBestTarget();
+        return target.getBestCameraToTarget();
+    }
+
+
 
 
     @Override
